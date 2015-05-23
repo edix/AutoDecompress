7798c091c0d4ba6ba3f06f98711680f907eaef60
edix <esmid.idrizovic@gmail.com>
2015-05-23 14:50:37 +0200

    + added aplib decompression (doing nothing with the data)

--- AutoDecompress/AutoDecompress.cpp
+++ AutoDecompress/AutoDecompress.cpp
@@ -15,28 +15,108 @@
 #include <auto.hpp>
 #include <entry.hpp>
 #include <bytes.hpp>
-//#include <typeinf.hpp>
 #include <funcs.hpp>
 
-#include <algorithm>
-
+#include "aplib/depacks.h"
 
 int idaapi init(void)
 {
-	if (ph.id != PLFM_386)
-	{
-		return PLUGIN_SKIP;
-	}
-
 	return PLUGIN_OK;
 }
 
-std::string GetFunctionName(ea_t ea)
+bool UnpackAplibAtAddress(ea_t address)
 {
-	qstring funcName;
-	get_func_name2(&funcName, ea);
-	return funcName.c_str();
+	struct APLIB_HEADER
+	{
+		uint32 tag;
+		uint32 header_size;
+		uint32 packed_size;
+		uint32 packed_crc;
+		uint32 original_size;
+		uint32 original_crc;
+	};
+
+	APLIB_HEADER header;
+	unsigned char* pDestination = nullptr;
+	unsigned char* pCompressed = nullptr;
+	uint32 uiCompressedSize, uiDecompressedSize;
+	uint uiOutputLength;
+	bool fResult = false;
+
+	const uint32 uiMaxCompressionSize = 0x100000;
+
+	if (get_many_bytes(address, &header, sizeof(header)))
+	{
+		if (header.tag == '23PA' && header.header_size == 24)
+		{
+
+			//
+			// ok valid AP32
+			// now get compressed buffer
+			//
+			uiCompressedSize = header.packed_size;
+			uiDecompressedSize = header.original_size;
+
+			//
+			// only 1 MB for now
+			//
+			if (uiCompressedSize <= uiMaxCompressionSize && uiDecompressedSize <= uiMaxCompressionSize)
+			{
+				pCompressed = new unsigned char[uiCompressedSize];
+				pDestination = new unsigned char[uiDecompressedSize];
+
+				if (pCompressed && pDestination)
+				{
+					//
+					// get compressed data
+					//
+					if (get_many_bytes(address + header.header_size, pCompressed, uiCompressedSize))
+					{
+						uiOutputLength = aP_depack_safe(pCompressed, uiCompressedSize, pDestination, uiDecompressedSize);
+						if (uiOutputLength != (uint)-1 && uiOutputLength != 0)
+						{
+							//
+							// ok, decompression is valid valid
+							//
+							msg("AutoDecompress: APLIB: decompressed %u bytes\n", uiOutputLength);
+
+							//
+							// todo: save somewhere :|
+							//
+
+							fResult = true;
+						}
+					}
+					else
+					{
+						msg("AutoDecompress: APLIB: can't read %u bytes at %a\n", uiCompressedSize, address);
+
+					}
+				}
+
+				delete[] pDestination;
+				delete[] pCompressed;
+			}
+			else
+			{
+				msg("AutoDecompress: APLIB: compress or decompress size is invalid (compress: %u bytes, decompress: %u bytes, max: %u bytes), aborting...\n", uiCompressedSize, uiDecompressedSize, uiMaxCompressionSize);
+			}
+
+		}
+		else
+		{
+			msg("AutoDecompress: APLIB invalid header.\n");
+		}
+	}
+
+	return fResult;
 }
+
+
+//void idaapi button_func(TView *fields[], int code)
+//{
+//	msg("The button was pressed!\n");
+//}
 
 
 void idaapi run(int)
@@ -46,9 +126,48 @@ void idaapi run(int)
 		return;
 	}
 
-	msg("AutoDecompress started.\n");
+	msg("AutoDecompress: started.\n");
+	
+	//
+	// select dialog for unpacking type
+	//
+	char *szDialogForm =
+		"STARTITEM 0\n"
+		"AutoDecompress\n\n"
+		"<Enter decryption key:A:16:16::>\n"					// unused right now, should be a XOR key or something...
+		"<##Please select your decryption method##APLIB:R>\n"
+		"<Shift left:R>\n"
+		"<Shift right:R>\n"
+		"<XOR:R>>\n";
 
+
+	char szKey[MAXSTR];
+	ushort uiSelect = 0;
+	qstrncpy(szKey, "", sizeof(szKey));
+
+	if (AskUsingForm_c(szDialogForm, szKey, &uiSelect/*, button_func*/) == 1)
+	{
+		ea_t address;
+		address = get_screen_ea();
+
+		switch (uiSelect)
+		{
+		case 0:
+			if (UnpackAplibAtAddress(address))
+			{
+				msg("AutoDecompress: unpacked data at address %a with aplib.\n", address);
+			}
+			else
+			{
+				msg("AutoDecompress: APLIB unpacking failed at %a\n.", address);
+			}
+			break;
+		default:
+			msg("unknown selection: %u\n", uiSelect);
+		}
+	}
 }
+
 
 plugin_t PLUGIN =
 {
